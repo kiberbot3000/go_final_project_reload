f99c3c428df7e75255b1ddf2c40a0a4628d6228d
kiberbot3000 <sandorr210@gmail..com>
2024-10-17 23:35:14 +0300

    6

--- dates/repeat.go
+++ dates/repeat.go
@@ -11,21 +11,24 @@ import (
 )
 
 func dailyPattern(now time.Time, startDate time.Time, repeat string) (string, error) {
+
 	days, err := strconv.Atoi(strings.TrimPrefix(repeat, "d "))
 	if err != nil {
 		log.Printf("Error parsing pattern value as int: %s\n", err)
 		return "", err
 	}
-	if days <= 0 || days > 365 {
+
+	if days <= 0 || days > 400 {
 		log.Printf("Invalid repetition range for 'd' pattern: %s\n", err)
 		err = errors.New("invalid repetition range for 'd' pattern")
 		return "", err
 	}
 
 	nextDate := startDate
+
 	nextDate = nextDate.AddDate(0, 0, days)
 
-	for now.After(nextDate) || nextDate.Equal(now) {
+	for now.After(nextDate) || nextDate == now {
 		nextDate = nextDate.AddDate(0, 0, days)
 	}
 
@@ -34,13 +37,15 @@ func dailyPattern(now time.Time, startDate time.Time, repeat string) (string, er
 
 func yearlyPattern(now time.Time, startDate time.Time) (string, error) {
 	nextDate := startDate.AddDate(1, 0, 0)
-	for now.After(nextDate) {
+
+	for now.After(nextDate) || nextDate == now {
 		nextDate = nextDate.AddDate(1, 0, 0)
 	}
 	return nextDate.Format(constants.DateFormat), nil
 }
 
 func NextDate(now time.Time, date string, repeat string) (string, error) {
+
 	startDate, err := time.Parse(constants.DateFormat, date)
 	if err != nil {
 		log.Printf("Task date is not in valid format: %s", err)
@@ -52,14 +57,11 @@ func NextDate(now time.Time, date string, repeat string) (string, error) {
 		return dailyPattern(now, startDate, repeat)
 	case repeat == "y":
 		return yearlyPattern(now, startDate)
-	case repeat == "w":
-		err = errors.New("weekly repetition is not supported")
-		return "", err
 	case repeat == "":
 		err = errors.New("no repetition range set")
 		return "", err
 	default:
-		err = errors.New("repetition pattern is invalid")
+		err = errors.New("repetition pattern is not supported")
 		return "", err
 	}
 }
